209c1c807b4d95dfc680f1c1b1db05fed41befdc
alejoboga20 <alejoboga19@gmail.com>
2024-11-13 15:57:04 -0300

    missing commands

--- app/main.py
+++ app/main.py
@@ -1,13 +1,34 @@
 import sys
 
+command_list: list[str] = ["pwd", "cd"]
+
+
+def get_user_input():
+    user_input: str = input()
+
+    return user_input
+
+
+def print_output(output: str):
+    sys.stdout.write(output)
+    sys.stdout.write("\n")
+
+
+def find_command(command: str):
+    if command in command_list:
+        return command
+    else:
+        return f"{command}: command not found"
+
 
 def main():
-    # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
-    # Wait for user input
-    input()
+    user_input = get_user_input()
+    result = find_command(user_input)
+
+    print_output(result)
 
 
 if __name__ == "__main__":
